ac1f66d824f242b93c235846dacc3d480772db64
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-10 14:06:48 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// подсчитать количество числа со значением 5 в одномерном массиве.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 3, 5, 7, 5, 2, 3, 4, 5, 6 };
+            //int count = 0;
+            //foreach(var element in omas)
+            //{
+            //    if(element==5)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+            int count = omas.Count(x => x == 5);
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+    }
+}
