3c10dcbd2d665946de0a19dda91e90b3edb9a772
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-10 13:37:52 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    /// <summary>
+    /// Сортировка одномерный массива по возрастанию и по убыванию.
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach(var element in people)
+            {
+                Console.WriteLine(element);
+            }
+            Array.Reverse(people);
+            foreach(var element in people)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
