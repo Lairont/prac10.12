bc6e745f9e72151b51d1324593c2298b24e6bed1
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-10 14:33:35 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 3, 5, 7, 4, 23, 4, 5, 7 };
+            int sum = omas.Sum();
+               Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
